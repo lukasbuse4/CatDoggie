55bcb65a1dcd1d9a341be781f2ff4e2a22b0d6a5
lukasbuse <lukasbuse4@gmail.com>
2022-07-15 16:31:58 +0300

    Initial Commit

---
+++ Shared/CatDoggieApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatDoggieApp.swift
+//  Shared
+//
+//  Created by Josh Buse on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct CatDoggieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
