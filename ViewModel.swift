7e789fc117d011f846c9b74d14bcd4a58aad7d8c
lukasbuse <lukasbuse4@gmail.com>
2022-07-17 09:55:09 +0300

    ViewModel (VM) complete communication between

---
+++ ViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  ViewModel.swift
+//  CatDoggie (iOS)
+//
+//  Created by Josh Buse on 7/17/22.
+//
+
+import Foundation
+import SwiftUI
+
+//The role of the ViewModel is to interpret the communication between the VIEW & MODEL
+    //Tells MODEL when user taps from View
+    //Tells view state of the Model
+
+
+class ViewModel{
+    var model:Model = Model()
+    //viewmodel have model 
+    
+    var pet:String{
+        return model.pet.rawValue
+        //returns Model Pet as a string
+    }
+    
+    func switchPet(){
+        model.switchPet()
+        //calls Model()
+    }
+    
+    
+}
